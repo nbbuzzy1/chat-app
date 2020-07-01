8806d3d2032e30992204894341667249e6d09457
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-07-01 14:19:01 -0400

    Counting websocket example.

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,10 @@
-io()
+const socket = io()
+
+document.getElementById('increment').addEventListener('click', () => {
+    socket.emit('increment')
+})
+
+
+socket.on('countUpdated', (count) => {
+    console.log('The count has been updated!', count)
+});
