8cf62175b5ce2abf63ddf32621be8b0d0c297745
nbbuzzy1 <40919504+nbbuzzy1@users.noreply.github.com>
2020-07-02 16:47:47 -0400

    Handle location with users.

--- public/js/chat.js
+++ public/js/chat.js
@@ -4,8 +4,22 @@ socket.on('message', (message) => {
     console.log(message)
 });
 
-document.getElementById('chatForm').addEventListener('submit', (e) => {
+document.getElementById('chat-form').addEventListener('submit', (e) => {
     e.preventDefault()
     const message = e.target.elements.message.value;
     socket.emit('sendMessage', message)
+});
+
+document.getElementById('send-location').addEventListener('click', () => {
+    if (!navigator.geolocation) {
+        return alert('Geolocation is not supported by your browser')
+    }
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const coordinates = {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }
+        socket.emit('sendLocation', coordinates)
+    })
 });
